b399c9a2dde92d72f186f65c70b37a846d4fe4ce
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-27 17:14:28 +0500

    `Cors added`

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,12 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
-
+import cors from 'cors'
 import { connectDB } from "./config/database.js";
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
