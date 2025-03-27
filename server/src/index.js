5dc74963739df32f24877e377a0b2fa3c901747f
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-27 15:09:36 +0500

    `Template for register and login routes added`

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,10 @@
-
-import express from 'express'
+import express from "express";
+import "dotenv/config";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-
-app.listen(3000,() => {
-    console.log("Server is running")
-})
+app.use("/api/auth", authRoutes);
+app.listen(3000, () => {
+  console.log("Server is running");
+});
