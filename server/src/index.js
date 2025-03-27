93d86c9ad5935a58670d9cffe5798877cd55ae90
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-27 15:32:23 +0500

    `Create New User Route Completed`

--- server/src/index.js
+++ server/src/index.js
@@ -4,8 +4,10 @@ import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./config/database.js";
 const app = express();
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.listen(3000, () => {
   console.log("Server is running");
-  connectDB
+  connectDB;
 });
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,41 @@
 import express from "express";
-
+import { userModel } from "../models/User.js";
 const router = express.Router();
 
-router.post("/register", async(req, res) => {
-  res.send("login");
+router.post("/register", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "Please fill in all fields" });
+    }
+
+    // check password length error!
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Username should be at least 3 character long!" });
+    }
+
+    // check if user already exists!
+    const isExists = await userModel.findOne({
+      $or: [{ email }, { username }]
+    });
+
+    if (isExists) {
+      return res
+        .status(400)
+        .json({ message: "User already exists with this email or username!" });
+    }
+    const user = await userModel.create({ email, username, password });
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
-router.post("/login", async(req, res) => {
+router.post("/login", async (req, res) => {
   res.send("login");
 });
 
