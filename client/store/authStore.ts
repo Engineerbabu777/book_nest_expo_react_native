57b26cb99a4ddb7805006bd509be147e5304b12c
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-05-03 18:43:33 +0500

    register methgod in zustand has been done

--- client/store/authStore.ts
+++ client/store/authStore.ts
@@ -1,8 +1,57 @@
-
 import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+interface AuthState {
+  user: any | null;
+  token: string | null;
+  isLoading: boolean;
 
-export const useAuthStore = create((set) = ({
-    user:{name:"john"},
+  register: (
+    username: string,
+    email: string,
+    password: string
+  ) => Promise<{ success: boolean }>;
+}
 
-    sayeHello : () => console.log("Sayeing hello")
-}))
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  token: null,
+  isLoading: false,
+
+  // REGISTER NEW USER!
+  register: async (username, email, password) => {
+    set({ isLoading: true });
+
+    try {
+      const response = await fetch("https://localhost:3000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, password }),
+      });
+
+      const data = await response.json();
+
+      await AsyncStorage.setItem("token", data?.token);
+      await AsyncStorage.setItem("user", JSON.stringify(data?.user));
+
+      // Example of updating store with response
+      set({
+        user: data.user,
+        token: data.token,
+        isLoading: false,
+      });
+
+      return {
+        success: true,
+      };
+    } catch (error) {
+      console.error("Registration failed", error);
+      set({ isLoading: false });
+
+      return {
+        success: false,
+      };
+    }
+  },
+}));
