ac0bd6e8455cd5ed960da4f4a2fff920f26a948f
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-05-04 17:33:30 +0500

    book title field and header for the create screen

--- client/app/(tabs)/create.tsx
+++ client/app/(tabs)/create.tsx
@@ -1,16 +1,80 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import { useRouter } from "expo-router";
+import styles from "../../assets/styles/create.style";
+import COLORS from "@/constants/colors";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 type Props = {};
 
 const create = (props: Props) => {
+  const [title, setTitle] = useState("");
+  const [caption, setCaption] = useState("");
+  const [rating, setRating] = useState(3);
+  const [image, setImage] = useState(null);
+  const [imageBase64, setImageBase64] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const pickImage = async () => {};
+  const handleSubmit = async () => {};
+
   return (
-    <View>
-      <Text>create</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{
+        flex: 1,
+      }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        contentContainerStyle={styles.container}
+        style={styles.scrollViewStyle}
+      >
+        <View style={styles.card}>
+          {/* Header! */}
+          <View style={styles.header}>
+            <Text style={styles.title}>Add Book Recommendation</Text>
+            <Text style={styles.subtitle}>
+              Share your favorite reads with others
+            </Text>
+          </View>
+
+          {/*  */}
+          <View style={styles.form}>
+            {/* Book Title! */}
+            <View style={styles.formGroup}>
+              <Text style={styles.label}>Book Title</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="book-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter book Title"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={title}
+                  onChangeText={setTitle}
+                  autoCapitalize="none"
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 export default create;
-
-const styles = StyleSheet.create({});
